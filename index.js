5b81bea6514dfd72b19d25f62ecde7e78bb0a36b
marcus oskarsson <marcusoskarsson@hotmail.com>
2022-09-14 15:34:43 +0200

    javascript instead of css solution for hamburger menu

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const hamburgerToggle = document.getElementById("hamburger-toggle")
 const hamburgerMenuOpen = document.getElementById("hamburger-menu-open")
 const hamburgerMenuClose = document.getElementById("hamburger-menu-close")
+const hamburgerMenu = document.getElementById("navigation-links-container")
 
 
 const toggleMenu = () => {
   hamburgerMenuOpen.classList.toggle("hidden")
   hamburgerMenuClose.classList.toggle("hidden")
+  hamburgerMenu.classList.toggle("hidden")
 }
 
 const menuToggle = hamburgerToggle.addEventListener("click", toggleMenu)
